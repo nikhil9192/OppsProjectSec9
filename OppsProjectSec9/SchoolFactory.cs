b59a11c42a3744b172998e51b0d3538a9762ff60
nikhil9192 <nikhilsinghtech2580@gmail.com>
2023-10-26 15:34:26 +0530

    Add project files.

---
+++ OppsProjectSec9/SchoolFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OppsProjectSec9
+{
+     
+        public class SchoolFactory
+        {
+            public Student CreateStudent(string name, string classSection)
+            {
+                return new Student { Name = name, ClassSection = classSection };
+            }
+
+            public Teacher CreateTeacher(string name, string classSection)
+            {
+                return new Teacher { Name = name, ClassSection = classSection };
+            }
+
+            public Subject CreateSubject(string name, string subjectCode, Teacher teacher)
+            {
+                return new Subject { Name = name, SubjectCode = subjectCode, Teacher = teacher };
+            }
+        }
+
+    }
